f6a0e582b266b7db4288649474e088430eb3107e
Alejandro Barrios <125711812+alejo-barrios@users.noreply.github.com>
2024-08-21 19:28:39 -0500

    Revert "feature/fixRoles"

--- projectClub/src/main/java/app/service/interfaces/PartnerService.java
+++
@@ -1,9 +0,0 @@
-package app.service.interfaces;
-
-import app.dto.PartnerDto;
-import app.dto.PersonDto;
-import app.dto.UserDto;
-
-public interface PartnerService {
-    public void createPartner(PartnerDto partnerDto, UserDto userDto, PersonDto personDto) throws Exception;
-}
